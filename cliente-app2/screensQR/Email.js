d3d90503ebf0bc8e3850eda551a405559b6c02c2
dan0407 <duque.murillas@gmail.com>
2024-10-16 18:54:55 -0500

    cupon

--- cliente-app2/screensQR/Email.js
+++ cliente-app2/screensQR/Email.js
@@ -3,25 +3,8 @@ import { router, socket } from '../routes.js'; // Importando router y socket
 export default function Email() {
 	const app = document.getElementById('app');
 	app.innerHTML = `
-        <h1>Congratulations!</h1>
-        <p>You have helped Mug! To claim your coupon, both participants need to fill out the following form:</p>
-
-        <form id="claimForm">
-            <div>
-                <label for="name1">Name (Player 1):</label>
-                <input type="text" id="name1" name="name1">
-            </div>
-            <br>
-            <div>
-                <label for="name2">Name (Player 2):</label>
-                <input type="text" id="name2" name="name2">
-            </div>
-            <br>
-            <button type="submit" id="submitForm">Submit</button>
-        </form>
-
-        <button id="nextPage">Go to QR Page</button>
-    `;
+  <h1>¡Felicitaciones!</h1>
+            <p>Tu cupón fue enviado a tu correo.</p> `;
 
 	document.getElementById('nextPage').addEventListener('click', () => {
 		socket.emit('changeScreen'); // Emitiendo el evento 'changeScreen' al servidor
