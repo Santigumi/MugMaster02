62a58392f576d705fa415a4151889cbf88865def
Santiago <santymillan95@gmail.com>
2024-11-23 17:34:44 -0500

    off alert

--- cliente-app2/screensQR/Consume.js
+++ cliente-app2/screensQR/Consume.js
@@ -78,23 +78,23 @@ export default function consume() {
                 <img src="https://www.groundstobrew.com/wp-content/uploads/2022/04/starbucks-iced-coffee-drinks-1-683x1024.jpg">
                 <p>Cold drinks</p>
             </button>
-            <button class="option" id="coffee">
+            <button class="option" id="Coffee">
                 <img src="https://www.javapresse.com/cdn/shop/articles/JavaPresse_166.webp?v=1715088531&width=780" alt="Coffee">
                 <p>Coffee</p>
             </button>
-            <button class="option" id="breakfast">
+            <button class="option" id="Breakfast">
                 <img src="https://globalassets.starbucks.com/digitalassets/products/food/SBX20210915_BaconCheddarEggSandwich.jpg?impolicy=1by1_tight_288" alt="Breakfast">
                 <p>Breakfast</p>
             </button>
-            <button class="option" id="cakepops">
+            <button class="option" id="Cakepops">
                 <img src="https://globalassets.starbucks.com/digitalassets/products/food/SBX20181129_BirthdayCakePop.jpg?impolicy=1by1_medium_630" alt="CakePops">
                 <p>CakePops</p>
             </button>
-            <button class="option" id="snacks">
+            <button class="option" id="Snacks">
                 <img src="https://globalassets.starbucks.com/digitalassets/products/food/SBX20190628_ShortbreadCookies.jpg?impolicy=1by1_tight_288" alt="Snacks">
                 <p>Snacks</p>
             </button>
-            <button class="option" id="hot-tea">
+            <button class="option" id="Hot-tea">
                 <img src="https://d2t88cihvgacbj.cloudfront.net/manage/wp-content/uploads/2020/12/Starbucks-Medicine-Ball-Tea-7.jpg?x29814" alt="Hot tea">
                 <p>Hot tea</p>
             </button>
@@ -116,7 +116,8 @@ export default function consume() {
 	document.getElementById('submit').addEventListener('click', () => {
 		if (selectedOption) {
 			socket.emit('submitForm', selectedOption);
-			alert(`You selected: ${selectedOption.replace(/-/g, ' ')}`);
+            alert(selectedOption)
+			// alert(`You selected: ${selectedOption.replace(/-/g, ' ')}`);
 			socket.emit('changeScreen');
 			router.navigateTo('/loading');
 			setTimeout(() => {
