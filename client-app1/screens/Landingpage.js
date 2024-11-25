e54e5cb6fef9cfc0c747fe1c4929ef8ad836d666
Santiago <santymillan95@gmail.com>
2024-11-25 16:56:11 -0500

    added div to css

--- client-app1/screens/Landingpage.js
+++ client-app1/screens/Landingpage.js
@@ -6,17 +6,7 @@ export default function landingPage() {
 	app.innerHTML = `
         <section>
         <img id="titule" src='../Resources/img/mug&master.png'>
-        <p>Participate with your partner and win a coupon for a combo for two</p>
         <p id="Play">Play</p>
         </section>
     `;
-
-	document.getElementById('nextPage').addEventListener('click', () => {
-		socket.emit('changeScreen');
-	});
-
-	/* socket.on("navigateTo", (screen) => {
-    console.log("navigateTo<Landingpage>:" + screen);
-    router.navigateTo(screen);
-  }); */
 }
