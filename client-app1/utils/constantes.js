64218f04a7645d2a7885668a22f87ef07838d89e
Santiago <santymillan95@gmail.com>
2024-11-29 10:34:37 -0500

    rules for demo

--- client-app1/utils/constantes.js
+++ client-app1/utils/constantes.js
@@ -1,9 +1,9 @@
 const IZQUIERDDA = 1;
 const ARRIBA = 2;
 const DERECHA = 3;
-const MAXERRORRES = 2;
-const MAXCICLOTIEMPO = 2000;
-const base1 = 0;
-const base2 = 0;
+const MAXERRORRES = 10;
+const MAXCICLOTIEMPO = 30;
+const base1 = 100;
+const base2 = 800;
 
 export { IZQUIERDDA, ARRIBA, DERECHA, MAXERRORRES, MAXCICLOTIEMPO, base1, base2 };
